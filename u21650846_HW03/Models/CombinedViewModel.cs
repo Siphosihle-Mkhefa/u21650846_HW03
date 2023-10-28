0182e522c8758e17d4cf524db0d2a6e652d10436
ThatgirlSipho <127086861+Siphosihle-Mkhefa@users.noreply.github.com>
2023-10-28 22:08:23 +0200

    Added the Viewodel with all data models

---
+++ u21650846_HW03/Models/CombinedViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace u21650846_HW03.Models
+{
+    public class CombinedViewModel
+    {
+        public IEnumerable<authors > authors { get; set; }
+        public IEnumerable<books > books { get; set; }
+        public IEnumerable<borrows> borrows { get; set; }
+        public IEnumerable <students > students { get; set; }
+        public IEnumerable < types > types { get; set; }
+    }
+}
