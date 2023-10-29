c1b7d19d7ba96c01b08a05ca29ae0408c10f7456
ThatgirlSipho <127086861+Siphosihle-Mkhefa@users.noreply.github.com>
2023-10-29 02:39:56 +0200

    Added the maintain tables. Basic lay

--- u21650846_HW03/Controllers/HomeController.cs
+++ u21650846_HW03/Controllers/HomeController.cs
@@ -12,12 +12,13 @@ namespace u21650846_HW03.Controllers
     public class HomeController : Controller
     {
         private LibraryEntities db = new LibraryEntities();
-        public ActionResult Index()
+        public async Task <ActionResult> Index()
         {
             var viewmodel = new CombinedViewModel()
             {
-                students= db.students.ToList(),
-                books= db.books.Include(a => a.authors).Include(t => t.types).ToList(),   
+                students= await db.students.ToListAsync(),
+                books= await db.books.Include(a => a.authors).Include(t => t.types).ToListAsync(),   
+                borrows= await db.borrows.ToListAsync(),
             };
             return View(viewmodel);
         }
---
+++ u21650846_HW03/Controllers/MaintainController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.Net;
+using u21650846_HW03.Models;
+
+namespace u21650846_HW03.Controllers
+{
+    public class MaintainController : Controller
+    {
+        // GET: Maintain
+        private LibraryEntities db = new LibraryEntities();
+        public async Task< ActionResult> Maintain()
+        {
+            var viewmodel = new CombinedViewModel()
+            {
+                authors= await db.authors.ToListAsync(),
+                types=await db.types.ToListAsync(),
+                borrows=await db.borrows.Include(b => b.books).Include(b => b.students).ToListAsync(),
+
+            };
+            return View();
+        }
+    }
+}
